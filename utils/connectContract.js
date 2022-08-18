8959acb4b4218a4917c38fe09a2d1f8fe00cdc50
hyperalchemy <23337041+hyperalchemy@users.noreply.github.com>
2022-08-18 18:04:18 +0200

    update

--- utils/connectContract.js
+++ utils/connectContract.js
@@ -3,19 +3,24 @@ import { ethers } from "ethers";
 
 function connectContract() {
   //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
-  const contractAddress = "0x[298ca57F4cb42d7BeCCCE7d93358297ed20C0064]";
+  const contractAddress = "0x298ca57F4cb42d7BeCCCE7d93358297ed20C0064";
   const contractABI = abiJSON.abi;
   let rsvpContract;
   try {
     const { ethereum } = window;
 
-    if (ethereum) {
-      //checking for eth object in the window
+    if (ethereum.chainId === "0x13881") {
+      //checking for eth object in the window, see if they have wallet connected to Mumbai network
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      console.log("contractABI", contractABI);
+      rsvpContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      ); // instantiating new connection to the contract
     } else {
-      console.log("Ethereum object doesn't exist!");
+      throw new Error('Please connect to the Polygon Mumbai network.')
     }
   } catch (error) {
     console.log("ERROR:", error);
